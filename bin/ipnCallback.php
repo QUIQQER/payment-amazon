1b29130c78033c1e124c6d1940ceefa99bfdc12d
Patrick Müller <p.mueller@pcsg.de>
2018-09-24 09:01:07 +0200

    feat: own callback handler for IPN requests

--- bin/ipnCallback.php
+++ bin/ipnCallback.php
@@ -7,7 +7,15 @@ require_once dirname(dirname(dirname(dirname(__FILE__)))).'/header.php';
 use \Symfony\Component\HttpFoundation\Response;
 use AmazonPay\IpnHandler;
 use QUI\ERP\Accounting\Payments\Transactions\Handler as TransactionsHandler;
+use QUI\ERP\Order\Handler as OrderHandler;
 
+function badRequest()
+{
+    $Response = QUI::getGlobalResponse();
+    $Response->setStatusCode(Response::HTTP_BAD_REQUEST);
+    $Response->send();
+    exit;
+}
 
 $headers = getallheaders();
 $body    = file_get_contents('php://input');
@@ -15,29 +23,56 @@ $body    = file_get_contents('php://input');
 try {
     $IpnHandler = new IpnHandler($headers, $body);
 } catch (\Exception $Exception) {
-    // request is not an Amazon IPN request and can be safely ignored
+    // request is not an Amazon IPN request and can be safely ignored here
     exit;
 }
 
 $ipnData = $IpnHandler->toArray();
 
-if (empty($ipnData['RefundDetails']['RefundReferenceId'])) {
-    // do nothing if the request was not a refund request
-    exit;
+// Handle Refund request
+if (!empty($ipnData['RefundDetails']['RefundReferenceId'])) {
+    try {
+        $AmazonPayment     = new \QUI\ERP\Payments\Amazon\Payment();
+        $refundReferenceId = $ipnData['RefundDetails']['RefundReferenceId'];
+        $transactionId     = $AmazonPayment->rebuildCroppedTransactionId($refundReferenceId);
+        $RefundTransaction = TransactionsHandler::getInstance()->get($transactionId);
+
+        $AmazonPayment->finalizeRefund($RefundTransaction, $ipnData);
+    } catch (\Exception $Exception) {
+        QUI\System\Log::writeException($Exception);
+        badRequest();
+    }
 }
 
-try {
-    $AmazonPayment     = new \QUI\ERP\Payments\Amazon\Payment();
-    $refundReferenceId = $ipnData['RefundDetails']['RefundReferenceId'];
-    $transactionId     = $AmazonPayment->rebuildCroppedTransactionId($refundReferenceId);
-    $RefundTransaction = TransactionsHandler::getInstance()->get($transactionId);
+// Handle Capture request
+if (!empty($ipnData['CaptureDetails']['CaptureReferenceId'])) {
+    try {
+        // parse Order ID from special reference ID
+        $orderIdentifier = explode('_', $ipnData['CaptureDetails']['CaptureReferenceId']);
+        $Orders          = OrderHandler::getInstance();
 
-    $AmazonPayment->finalizeRefund($RefundTransaction, $ipnData);
-} catch (\Exception $Exception) {
-    QUI\System\Log::writeException($Exception);
+        try {
+            $Order = $Orders->getOrderById($orderIdentifier[1]);
+        } catch (\Exception $Exception) {
+            QUI\System\Log::addError(
+                'Amazon Pay :: Could not load Order from IPN request. Parsed Order ID: '.$orderIdentifier[1]
+            );
 
-    $Response = QUI::getGlobalResponse();
-    $Response->setStatusCode(Response::HTTP_BAD_REQUEST);
-    $Response->send();
-    exit;
+            throw $Exception;
+        }
+
+        $Gateway = new QUI\ERP\Accounting\Payments\Gateway\Gateway();
+
+        $Gateway->setOrder($Order);
+        $Gateway->executeGatewayPayment();
+    } catch (\Exception $Exception) {
+        QUI\System\Log::writeException($Exception);
+        badRequest();
+    }
 }
+
+$Response = QUI::getGlobalResponse();
+$Response->setStatusCode(Response::HTTP_OK);
+$Response->send();
+
+exit;
